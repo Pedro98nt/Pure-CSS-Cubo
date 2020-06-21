575e9c6aaacc11c98f99497ec11edf16c0fa4bef
Pedro Soares <pedroneto.ps180@gmail.com>
2020-06-21 12:12:37 -0300

    cubo css

---
+++ script.js
@@ -0,0 +1,12 @@
+CSS.registerProperty({
+    name: '--f',
+    syntax: '<integer>',
+    initialValue: 0,
+    inherits: true });
+  
+  
+  CSS.registerProperty({
+    name: '--sr',
+    syntax: '<number>',
+    initialValue: 0,
+    inherits: true });
